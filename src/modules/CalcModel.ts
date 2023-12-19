76b67536a7063a31c2e241e6a8982f881a34406c
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2023-12-19 09:44:59 +0000

    add clear() and error handling to CalcModel

--- src/modules/CalcModel.ts
+++ src/modules/CalcModel.ts
@@ -11,10 +11,11 @@ interface Operation {
 
 export class CalcModel {
     history: Array<Entry>;
+    inError: boolean;
     ops: Map<string, Operation>;
 
     constructor() {
-        this.history = [];
+        this.clear();
         this.ops = new Map<string, Operation>();
         this.pushOp({name: '+', arity: 2, op: (a: number, b: number): number => a + b});
         this.pushOp({name: '-', arity: 2, op: (a: number, b: number): number => a - b});
@@ -41,39 +42,65 @@ export class CalcModel {
     }
 
     evaluate(): number {
-        const entry = this.history.pop();
+        const entry     = this.history.pop();
+        let   theResult = 0;
         if (entry && entry.type == 'number') { // arity = 2
             const right   = parseFloat(entry.value);
             const opEntry = this.history.pop();
             if (!opEntry || opEntry.type != 'operation') {
-                return 0;
+                this.error();
+                return theResult;
             }
             const leftEntry = this.history.pop();
             if (!leftEntry || leftEntry.type != 'number') {
-                return 0;
+                this.error();
+                return theResult;
             }
             const left = parseFloat(leftEntry.value);
             const op   = this.ops.get(opEntry.value);
             if (!op || op.arity != 2) {
-                return 0;
+                this.error();
+                return theResult;
             }
-            const theResult = op.op(left, right);
+            theResult = op.op(left, right);
             this.pushEntry({type: 'number', value: theResult.toString()});
             return theResult;
         } else if (entry && entry.type == 'operation') { // arity = 1
             const op = this.ops.get(entry.value);
             if (!op || op.arity != 1) {
-                return 0;
+                this.error();
+                return theResult;
             }
             const numEntry = this.history.pop();
             if (!numEntry || numEntry.type != 'number') {
-                return 0;
+                this.error();
+                return theResult;
             }
-            const num       = parseFloat(numEntry.value);
-            const theResult = op.op(num);
+            const num = parseFloat(numEntry.value);
+            theResult = op.op(num);
             this.pushEntry({type: 'number', value: theResult.toString()});
             return theResult;
-        } 
-        return 0;
+        }
+        this.error();
+        return theResult;
+    }
+
+    topEntry(): Entry {
+        return this.history[this.history.length-1];
+    }
+
+    clear(): void {
+        this.history = [];
+        this.pushEntry({type: 'number', value: '0'});
+        this.inError = false;
+    }
+
+    error(): void {
+        this.clear();
+        this.inError = true;
+    }
+
+    hasError(): boolean {
+        return this.inError;
     }
 }
